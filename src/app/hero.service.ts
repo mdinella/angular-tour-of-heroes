bdc4c70455a51a6ccad002f781392f6c1b7dd468
mdinella <dinellamatteo@gmail.com>
2021-10-26 17:02:03 +0200

    add navigation with router-links, configured the router in an AppRoutingModule,  shared HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
+
 import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
   constructor(private messageService: MessageService) {}
 
@@ -15,5 +14,11 @@ export class HeroService {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
