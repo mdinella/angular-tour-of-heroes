a2bb6cc325c0722aa177978d07c8ff4776ff0ca6
mdinella <dinellamatteo@gmail.com>
2021-10-26 15:32:35 +0200

    implement hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
-
-  constructor() { }
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
+  }
 }
